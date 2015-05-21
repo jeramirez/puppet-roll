43d9ad6413fb82378eb38b7367f49efea9363d47
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2015-05-21 18:20:01 -0400

    added src/usersguide

---
+++ src/usersguide/version.mk
@@ -0,0 +1,12 @@
+ROLL			= puppet
+VERSION			= 3.7.5
+NAME    		= roll-$(ROLL)-usersguide
+RELEASE			= 0
+
+SUMMARY_COMPATIBLE	= $(VERSION)
+SUMMARY_MAINTAINER	= Rocks Group
+SUMMARY_ARCHITECTURE	= i386, x86_64
+
+ROLL_REQUIRES		= base kernel os
+ROLL_CONFLICTS		=
+
